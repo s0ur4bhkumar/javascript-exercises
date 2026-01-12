8bcc811ba1dd833d4228e1616d374f5d5f72a3c0
s0ur4bhkumar <soul65917@gmail.com>
2026-01-12 20:09:35 +0530

    complete foundations06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let repeatStr = "";
+  if (num < 0) {
+    return "ERROR";
+  } else if (string === "" || num === 0) {
+    return "";
+  } else {
+    for (let i = 1; i <= num; i++) {
+      repeatStr += string;
+    }
+    return repeatStr;
+  }
 };
 
 // Do not edit below this line
