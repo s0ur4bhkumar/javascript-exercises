1c30875d19e6a17161afe0fb4c4e87d6d4717f3e
s0ur4bhkumar <soul65917@gmail.com>
2026-01-12 20:19:19 +0530

    complete foundations07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reverseStr = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverseStr += string[i];
+  }
+  return reverseStr;
 };
 
 // Do not edit below this line
