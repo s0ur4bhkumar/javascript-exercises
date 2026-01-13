21964f6fba5042f826a82a53ae7801d6fa428459
s0ur4bhkumar <soul65917@gmail.com>
2026-01-13 13:48:31 +0530

    complete foundations09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  } else if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  }
+  for (let i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
