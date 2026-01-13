485b52292a7eaed231b2b90a0e21ce44790ba89b
s0ur4bhkumar <soul65917@gmail.com>
2026-01-13 15:31:13 +0530

    complete foundations11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  return roundTo((5 / 9) * (temp - 32), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  return roundTo((9 / 5) * temp + 32, 1);
 };
+
+function roundTo(num, precision) {
+  const factor = Math.pow(10, precision);
+  return Math.round(num * factor) / factor;
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
+  roundTo,
 };
