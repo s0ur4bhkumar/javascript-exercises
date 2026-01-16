dfd01b475efbe38637e1fbd2b43f8d22afbe1b0c
s0ur4bhkumar <soul65917@gmail.com>
2026-01-16 14:49:46 +0530

    complete foundations-13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let reverse = "";
+  let newStr = str.replace(/[, .!]+/g, "").toLowerCase();
+  for (let i = newStr.length - 1; i >= 0; i--) {
+    reverse += newStr[i];
+  }
+  if (reverse === newStr) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
