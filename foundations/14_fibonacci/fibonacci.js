e4f23c9025e241e3b31984c6dc35aabc1284da47
s0ur4bhkumar <soul65917@gmail.com>
2026-01-16 15:35:36 +0530

    complete foundations-14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  let a = 0,
+    b = 1;
+  let c = 0;
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1 || n == 2) {
+    return 1;
+  }
+  if (n < 0) {
+    return "OOPS";
+  }
+  for (let i = 1; i <= n; i++) {
+    [a, b] = [b, c];
+    c = a + b;
+  }
+  return c;
 };
 
 // Do not edit below this line
