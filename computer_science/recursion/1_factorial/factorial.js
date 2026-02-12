26905aaf18ce511c7e46a76312e1a34952b09fda
s0ur4bhkumar <soul65917@gmail.com>
2026-02-12 15:52:24 +0530

    complete recurssion factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,13 @@
-const factorial = function() {
-
+const factorial = function (num) {
+  if (num >= 0 && Number.isInteger(num)) {
+    if (num === 0) {
+      return 1;
+    } else {
+      return num * factorial(num - 1);
+    }
+  } else {
+    return undefined;
+  }
 };
 
 // Do not edit below this line
