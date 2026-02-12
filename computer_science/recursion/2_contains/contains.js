cbdcad1d4a91f6517bbf9ba8ffc1dcbf635b0b89
s0ur4bhkumar <soul65917@gmail.com>
2026-02-12 16:56:22 +0530

    complete recurssion contains (with help :(  )

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,14 @@
-const contains = function() {
-  
+const contains = function (obj, val) {
+  const values = Object.values(obj);
+
+  if (values.includes(val)) return true;
+
+  const nestedObjects = values.filter(
+    (nestedObj) => typeof nestedObj === "object" && nestedObj !== null,
+  );
+
+  return nestedObjects.some((nestedObj) => contains(nestedObj, val));
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
