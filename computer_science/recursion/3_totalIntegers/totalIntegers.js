244014affc1080d53dd7ba158127dc1148700c8c
s0ur4bhkumar <soul65917@gmail.com>
2026-02-13 17:19:57 +0530

    complete totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,18 @@
-const totalIntegers = function() {
-  
-};
-  
+function totalIntegers(lst) {
+  let count = 0;
+  const keys = Object.keys(lst);
+  if (typeof lst !== "object") {
+    return undefined;
+  }
+  for (idx of keys) {
+    if (Number.isInteger(lst[idx])) {
+      count += 1;
+    } else if (typeof lst[idx] === "object" && lst[idx] !== null) {
+      count = count + totalIntegers(lst[idx]);
+    }
+  }
+  return count;
+}
+
 // Do not edit below this line
 module.exports = totalIntegers;
