21245b9401a206ae112f85ee52f6b505422c318b
s0ur4bhkumar <soul65917@gmail.com>
2026-02-14 20:11:56 +0530

    complete pascal assignment (and I did it myself :D)

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,17 @@
-const pascal = function() {
-  
+const pascal = function(num) {
+  let result = [];
+  if (num === 1) {
+    return [1];
+  }
+  if (num === 2) {
+    return [1, 1];
+  }
+  for (let i = 0; i < num - 2; i++) {
+    result.push(pascal(num-1)[i] + pascal(num-1)[i + 1]);
+  }
+  result.unshift(1);
+  result.push(1);
+  return result;
 };
   
 // Do not edit below this line
