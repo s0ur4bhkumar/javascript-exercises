b47c418fc393727cf2c9837628c38eb63c9e03f7
s0ur4bhkumar <soul65917@gmail.com>
2026-02-14 16:59:44 +0530

    complete permutations (i am not proud at all)

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,27 @@
-const permutations = function() {
-  
+const permutations = function (arr) {
+  if (arr.length === 0) {
+    return [[]];
+  }
+
+  if (arr.length === 1) {
+    return [arr];
+  }
+
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+
+    let remaining = arr.slice(0, i).concat(arr.slice(i + 1));
+
+    let perms = permutations(remaining);
+
+    for (ele of perms) {
+      result.push([current].concat(ele));
+    }
+  }
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
